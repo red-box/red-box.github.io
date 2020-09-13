89071ebd02688b405f8e5c79643684b3caa08b90
red-box <42344808+red-box@users.noreply.github.com>
2020-09-13 17:48:57 +0200

    Live Test

--- js/menuNavigation.js
+++ js/menuNavigation.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  //sectionSelect('landingPage');
+  sectionSelect('landingPage');
   $('.menuWrapper').on('click',function(){
     $('.menuBar').toggleClass('menuToggle');
     menuToggle();
